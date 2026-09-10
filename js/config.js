88d86ba2c0eab51fc4ab78e3bd4122423484a7b7
TechKnowBuddy <80600071+ckevac@users.noreply.github.com>
2026-09-09 22:25:23 -0400

    Update config.js
    fixing web app url

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Paste the /exec URL from your Apps Script deployment here.
 // (Deploy > New deployment > Web app, in the Apps Script editor.)
 const APP_CONFIG = {
-  API_URL: https://script.google.com/macros/s/AKfycbxGhvdczjZ2sncOCoYvRtJG0XxdmKKllJ9MM7Uyk3ITAtwq1jVEvCaBzMUGSK5-v9-uTg/exec
+  API_URL: 'https://script.google.com/macros/s/AKfycbxGhvdczjZ2sncOCoYvRtJG0XxdmKKllJ9MM7Uyk3ITAtwq1jVEvCaBzMUGSK5-v9-uTg/exec'
 };
